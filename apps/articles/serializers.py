14c1050e9991af25f06b008e7d684f4f1a429fef
SANJIKS <sanzarmaratov588@gmail.com>
2023-05-09 19:44:55 +0600

    fix serializer

--- apps/articles/serializers.py
+++ apps/articles/serializers.py
@@ -25,6 +25,7 @@ class ArticleSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Article
+        fields = '__all__'
         read_only_fields = ['user', 'slug']
         list_serializer_class = ArticleListSerializer
 
